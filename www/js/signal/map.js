66033562371a1a8dbd13ca2d1573ca5fd402dd8d
Grady Morgan <gradymorgan@gmail.com>
2015-11-04 19:15:55 -0800

    add target angle reference lines to tack map.

--- www/js/signal/map.js
+++ www/js/signal/map.js
@@ -45,15 +45,21 @@ var mapView = Backbone.View.extend({
 
         return [projection, trackPath];
     },
-    renderAnnotations: function(svg, angle) {
+    renderAnnotations: function(svg, angle, windSpeed) {
         var annotationsLayer= svg.append("g")
                 .attr('class', 'annotations')
 
             // wind
-            var wind = annotationsLayer
+            var windHolder = annotationsLayer
                         .append("g")
                             .attr('transform', 'translate(500, 50)')
-                        .append("g")
+                        
+            windHolder.append('text')
+                .attr('dy', -15)
+                .style('text-anchor', 'middle')
+                .text(windSpeed.toFixed(0) + ' kts');
+
+            var wind = windHolder.append("g")
                             .attr("class", "wind")
                             .attr('transform', 'rotate(180)')
 
@@ -196,7 +202,7 @@ var mapView = Backbone.View.extend({
 
         
         if ( this.annotations ) {
-            this.renderAnnotations(svg, angle);
+            this.renderAnnotations(svg, angle, this.model.data[0].tws);
         }
 
         // world
@@ -281,11 +287,16 @@ var mapView = Backbone.View.extend({
             var index = _.sortedIndex( view.model.data, {t: time}, function(point) { return point.t; } );
             var point = view.model.data[index];
 
+            //TODO: cleanup
+            if ( !point ) 
+                return;
+
             var coord = projection([point.lon, point.lat]);
             
             boat.attr('transform', 'translate('+(coord[0])+","+(coord[1]) +")scale(.06)rotate("+point.hdg+",-10,-10)");
 
             //TODO: smooth the TWD
+            //TODO: update TWS
             if ( 'twd' in point ) {
                 svg.select('g.wind').attr('transform', 'rotate('+ (180-angle+point.twd) +')');
             }
@@ -305,9 +316,11 @@ var mapView = Backbone.View.extend({
             $('.layer.'+layerName).show();
             return false;
         })
-            .eq(1).click(); //select performance
+            .eq(0).click(); //select tacks
     },
     renderScrubber: function(width, height) {
+        var view = this;
+
         //set up background color blocks
         var maneuvers = this.model.maneuvers;
         this.legs = [];
@@ -346,6 +359,9 @@ var mapView = Backbone.View.extend({
 
         var classes = function(d) {
             var c = ['board'];
+
+            if ( d.board == 'PS' ) 
+                c.push('prestart');
 
             if ( d.board.charAt(0) == 'D' )
                 c.push('downwind');
@@ -396,16 +412,23 @@ var mapView = Backbone.View.extend({
                 .attr("transform", "translate(0,-10)")
                 .call(xAxis);
 
-        scrubSvg.append('path')
-            .attr('d', 'M0,-80 C60,0 50,50 35,80 L-35,80 C-50,50 -60,0 0, -80')
+        var boat = scrubSvg.append('path')
+            .attr('d', 'M0,-8 C6,0 5,5 3.5,8 L-3.5,8 C-5,5 -6,0 0,-8')
             .attr('class', 'boat')
-            .attr('transform', 'scale(.12)');
+            .attr('transform', 'translate(0,5)');
                 
 
         scrubSvg.on('mousemove', function(a,b,c,d) {
             var pos = d3.mouse(this);
             var time = x.invert(pos[0]);
             app.trigger('scrub', new Date(time), pos);
+        });
+
+        // //listen to app events
+        this.listenTo(app, 'scrub', function(time) {
+            var boatPos = x(time);
+            
+            boat.attr('transform', "translate("+boatPos+",5)");
         });
 
         function brushed() {
@@ -450,27 +473,7 @@ var tackMapView = Backbone.View.extend({
 
         this.margin = {top: 0, right: 0, bottom: 0, left: 0};
     },
-    getProjection: function() {
-
-    },
-    render: function() {
-        var view = this;
-        
-        var margin = this.margin;
-        var width = this.$el.width() - margin.left - margin.right,
-            height = this.$el.height() - margin.top - margin.bottom;
-
-        width = width || 400;
-        height = height || 400;
-
-        // get extent of track, and make GEOJSON object
-        var allTimeRange = d3.extent(this.model.data, function(d) { return d.t; });
-        var lonExtent = d3.extent(this.model.data, function(d) { return d.lon; });
-        var latExtent = d3.extent(this.model.data, function(d) { return d.lat; });
-
-        var track = {type: "LineString", coordinates: _.compact( _.map(this.model.data, function(d) { return [d.lon, d.lat] }) ) };
-
-
+    getProjection: function(track, angle, width, height) {
         //create 'unit' projection
         var projection = d3.geo.mercator()
             .scale(1)
@@ -485,10 +488,8 @@ var tackMapView = Backbone.View.extend({
         var projectionScale = 1 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
 
 
-        // make the TWD at the start "UP"
         // calculate bounding rect for current track rotated
-        // and scale so it fits in current rect
-        var angle = this.model.up || parseInt(refTws(this.model.data)) || 0;
+        // and scale so it fits in current rect        
         var refAngle = angle % 180;
         if (refAngle > 90 ) refAngle = 180 - refAngle;
         var t = refAngle * Math.PI / 180;
@@ -502,7 +503,64 @@ var tackMapView = Backbone.View.extend({
 
         projection
             .scale(projectionScale*scale)
-            .translate(projectionTranslation)
+            .translate(projectionTranslation);
+
+        return [projection, trackPath];
+    },
+    //target wind angle reference lines serve to give tack path context
+    renderTWARefs: function(svg, targetAngle, projection) {
+
+        
+
+        var rad = function rad(degrees) {
+            return degrees * Math.PI / 180;
+        };
+
+        var references = [];
+        _.each([90-targetAngle, -1*(90-targetAngle)], function(refAngle, i) {
+            var reff = _.times(30, function(n) { 
+                var start = [0, -480 + 80*n];
+
+                var ytan = Math.tan(rad(refAngle)) * 400;
+                var end = [start[0] + 400, start[1] - ytan];
+                return start.concat(end);
+            });
+
+            references = references.concat(reff);
+        });
+
+        svg.selectAll('line.twa-ref')
+            .data(references)
+            .enter().append('line')
+                .attr('class', 'twa-ref')
+                .attr({"x1": function(d) { return d[0]; }, "x2": function(d) { return d[2]; }, "y1": function(d) { return d[1]; }, "y2": function(d) { return d[3]; }});
+    },
+    render: function() {
+        var view = this;
+
+        var mapAngle = this.model.up || parseInt(refTws(this.model.data)) || 0;
+        var track = {type: "LineString", coordinates: _.compact( _.map(this.model.data, function(d) { return [d.lon, d.lat] }) ) };
+
+
+        var margin = this.margin;
+        var width = this.$el.width() - margin.left - margin.right,
+            height = this.$el.height() - margin.top - margin.bottom;
+
+        width = width || 400;
+        height = height || 400;
+
+        // get extent of track, and make GEOJSON object
+        var allTimeRange = d3.extent(this.model.data, function(d) { return d.t; });
+        var lonExtent = d3.extent(this.model.data, function(d) { return d.lon; });
+        var latExtent = d3.extent(this.model.data, function(d) { return d.lat; });
+
+
+        
+
+        var res = this.getProjection(track, mapAngle, width, height);
+        var projection = res[0];
+        var trackPath = res[1];
+
 
 
         // svg container
@@ -518,19 +576,25 @@ var tackMapView = Backbone.View.extend({
             .attr("width", width + margin.left + margin.right);
 
 
-        // track
+
+        // rotated and scaled world container
         var world = svg.append('g')
             .attr('class', 'world')        
-            .attr('transform', function() { return "rotate(-"+angle+"," + (width / 2) + "," + (height / 2) + ")" })
+            .attr('transform', function() { return "rotate(-"+mapAngle+"," + (width / 2) + "," + (height / 2) + ")" })
         
-          
+        
+        this.renderTWARefs(svg, tack.targetAngle, projection);
+        
+        
+        //draw track
         world.append('path')
             .attr('class', 'track')
             .attr('d', trackPath(track))
 
-        // draw circles every 10 seconds, as tick marks on the track
+        // draw circles every n seconds, as tick marks on the track
+        var n = 1*1000;
         if ( this.circles ) {
-            var circles = _.filter(this.model.data, function(m) { return (Math.round((m.t - view.circles)/1000) % 1) === 0 });    
+            var circles = _.filter(this.model.data, function(m) { return (Math.round((m.t - view.circles)/n) % 1) === 0 });    
 
             world.selectAll('circle.timing')
                 .data(circles)
@@ -542,50 +606,9 @@ var tackMapView = Backbone.View.extend({
                 .style('stroke', function(d) { return (d.t - view.circles) === 0?'#f66':'#666'; });
         }
 
-        function proj(φ1, λ1, hdg) {
-            var d = 1;
-            var R = 3440.06479;
-            var brng = hdg * Math.PI / 180;
-            φ1 = φ1 * Math.PI / 180;
-            λ1 = λ1 * Math.PI / 180;
-            var φ2 = Math.asin( Math.sin(φ1)*Math.cos(d/R) +
-                    Math.cos(φ1)*Math.sin(d/R)*Math.cos(brng) );
-            var λ2 = λ1 + Math.atan2(Math.sin(brng)*Math.sin(d/R)*Math.cos(φ1),
-                         Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ2));
 
-            return [(λ2*180/Math.PI + 360) % 360, (φ2*180/Math.PI + 360) % 360];
-        }
-
-        if ( this.references ) {
-
-            var lines = _.map(this.references, function(ref) {
-                var start = proj(ref.lat, ref.lon, ref.hdg);
-                var s = projection(start);
-                var end = proj(ref.lat, ref.lon, ref.hdg+180);
-                var e = projection(end);
-                return s.concat(e);
-            });
-
-            world.selectAll('line.hdg')
-                .data(lines)
-                .enter().append('line')
-                    .attr('class', 'hdg')
-                    .attr({"x1": function(d) { return d[0]; }, "x2": function(d) { return d[2]; }, "y1": function(d) { return d[1]; }, "y2": function(d) { return d[3]; }})
-                    .style('stroke', '#666')
-                    .style('stroke-width', 0.25);
-
-            world.selectAll('circle.timing2')
-                .data(this.references)
-            .enter().append("circle")
-                .attr('class', 'timing')
-                .attr('r', '3')
-                .attr('cx', function(d) { return projection([d.lon, d.lat])[0] })
-                .attr('cy', function(d) { return projection([d.lon, d.lat])[1] })
-                .style('stroke', 'blue')
-                .style('stroke-width', 1);
-        }
-
-
+        
+        
         //create boat and put at start of race
         var start = projection(track.coordinates[0]);
         var hdg = view.model.data[0].hdg || 0;
@@ -594,40 +617,6 @@ var tackMapView = Backbone.View.extend({
             .attr('class', 'boat')
             .attr('transform', 'translate('+start[0]+','+start[1]+')scale(.06)rotate('+(hdg)+',-10,-10)')
 
-        if ( !this.events ) {
-            return;
-        }
-
-        // //listen to app events
-        this.listenTo(app, 'scrub', function(time) {
-            var index = _.sortedIndex( view.model.data, {t: time}, function(point) { return point.t; } );
-            var point = view.model.data[index];
-
-            var coord = projection([point.lon, point.lat]);
-            
-            boat.attr('transform', 'translate('+(coord[0])+","+(coord[1]) +")scale(.06)rotate("+point.hdg+",-10,-10)")
-        });
-
-        this.listenTo(app, 'zoom', function(start, end) {
-            // if ( start - allTimeRange[0] != 0 ) start -= 60000
-            // if ( end - allTimeRange[1] != 0 ) end = 60000 + end.getTime() //can't add date and int, but can subtract...
-            
-            // var trackPart = {type: "LineString", coordinates: _.compact( _.map(this.model.data, function(d) { if(d.t >= start && d.t <= end) return [d.lon, d.lat] }) ) };
-            
-            // var center = trackPath.centroid(trackPart);
-            // var partBounds = trackPath.bounds(trackPart);
-            // var bounds = trackPath.bounds(track);
-
-            // var scale = Math.max( Math.abs((bounds[1][0] - bounds[0][0])/(partBounds[1][0] - partBounds[0][0])), Math.abs((bounds[1][1] - bounds[0][1])/(partBounds[0][1] - partBounds[1][1])) );
-            // console.info('scale', scale, (bounds[1][0] - bounds[0][0]), (partBounds[1][0] - partBounds[0][0]), (bounds[1][1] - bounds[0][1]), (partBounds[0][1] - partBounds[1][1]));
-
-            // center[0] = (width/2 - center[0]) / scale;
-            // center[1] = (height/2 - center[1]) / scale;
-            // svg.selectAll('.world')
-            //     .attr('transform', "rotate(-"+angle+"," + (width / 2) + "," + (height / 2) + ")translate("+center[0]+","+center[1]+")scale("+scale+")" )
-
-            
-        });
     },
     onSelect: function(range) {
 
